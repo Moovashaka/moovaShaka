b1b6c54f651fde561d9fa3157fb38aea607479d1
Moova Shaka <mrmoovashaka@gmail.com>
2018-02-15 13:03:21 +0000

    phone link change

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -151,7 +151,7 @@ export default class Main extends React.Component {
                 </div>
                 <div className="col-lg-4 col-lg-offset-2 text-center">
                     <i className="fa fa-phone fa-3x sr-contact"></i>
-                    <p>0203 0123 231</p>
+                    <a href="tel:0203-0123-231">0203 0123 231</a>
                 </div>
                 <div className="col-lg-4 text-center">
                     <i className="fa fa-envelope-o fa-3x sr-contact"></i>
